9dcf7201427358ef60d880d82f35537f7c950c9e
ChenLiang <236338364@qq.com>
2018-09-17 03:45:00 +0800

    vueMall

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# StudyVueMall
-学习使用Vue开发一个移动电商
+# mall
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+
+# ToDo
+- babel-plugin-import
+- 移动web布局适配方法
+- vue-awesome-swiper
---
+++ src/components/pages/ShoppingMall.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="container">
+    <div class="search-bar">
+      <van-row gutter="5">
+        <van-col span="3">
+          <img :src="locationIcon" width="80%" class="location-icon">
+        </van-col>
+        <van-col span="16">
+          <input type="text" class="search-input">
+        </van-col>
+        <van-col span="5">
+          <van-button size="mini">查找</van-button>
+        </van-col>
+      </van-row>
+    </div>
+    <div class="swiper-area">
+      <van-swipe :autoplay="1000">
+        <van-swipe-item v-for="(banner, index) in bannerPicArray" :key="index">
+          <img v-lazy="banner.image" width="100%"/>
+        </van-swipe-item>
+      </van-swipe>
+    </div>
+    <div class="type-bar">
+      <div v-for="(cate, index) in category" :key="index" >
+        <img v-lazy="cate.image" width="90%" />
+        <span>{{cate.mallCategoryName}}</span> 
+      </div>
+    </div>
+    <div>
+      <img v-lazy="adBanner.PICTURE_ADDRESS" width="100%">
+    </div>
+    <div class="recommend-area">
+      <div class="recommend-title">
+          商品推荐
+      </div>
+      <div class="recommend-body">
+        <swiper :options="swiperOption">
+          <swiper-slide v-for="(item, index) in recommendGoods" :key="index">
+            <div class="recommend-item">
+              <img :src="item.image" width="80%" />
+              <div>{{item.goodsName}}</div>
+              <div>￥{{item.price}} (￥{{item.mallPrice}})</div>
+            </div>
+          </swiper-slide>
+        </swiper>
+      </div>
+    </div>
+    <swiperDefault></swiperDefault>
+  </div>
+</template>
+
+<script>
+  import 'swiper/dist/css/swiper.css'
+  import {swiper, swiperSlide} from 'vue-awesome-swiper'
+  import swiperDefault from 'components/swiper/swiperDefault'
+  export default {
+    data() {
+      return {
+        swiperOption:{
+          slidesPerView:3
+        },
+        locationIcon: require('assets/images/location.png'),
+        bannerPicArray: [],
+        category: [],
+        adBanner: {},
+        recommendGoods: []
+      }
+    },
+    created() {
+      this.$http.get('https://www.easy-mock.com/mock/5b9e9c792b292b0e9154c6cd/vueMall')
+        .then(response => {
+          if(response.status === 200){
+            this.bannerPicArray = response.data.data.slides
+            this.category = response.data.data.category
+            this.adBanner = response.data.data.advertesPicture
+            this.recommendGoods = response.data.data.recommend
+          }
+        })
+        .catch(err => {
+
+        })
+    },
+    components: {
+      swiper,
+      swiperSlide,
+      swiperDefault
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    .search-bar {
+      height: 2.2rem;
+      background-color: #e5017d;
+      line-height: 2.2rem;
+      .search-input {
+        width: 100%;
+        height: 1.3rem;
+        border-top:0px;
+        border-left:0px;
+        border-right:0px;
+        border-bottom: 1px solid 1px !important ;
+        background-color: #e5017d;
+        color:#fff;
+      }
+      .location-icon {
+        padding-top: .2rem;
+        padding-left: .3rem;
+      }
+    }
+    .swiper-area {
+      clear: both;
+      max-height: 15rem;
+      overflow: hidden;
+    }
+    .type-bar {
+      background-color: #fff;
+      margin:0 .3rem .3rem .3rem;
+      border-radius: .3rem;
+      font-size:14px;
+      display: flex;
+      flex-direction:row;
+      flex-wrap:nowrap;
+      div {
+        padding: .3rem;
+        font-size: 12px;
+        text-align: center;
+        flex:1;
+      }
+    }
+    .recommend-area {
+      background-color: #fff;
+      margin-top: .3rem;
+      .recommend-title {
+        border-bottom:1px solid #eee;
+        font-size:14px;
+        padding:.2rem;
+        color:#e5017d;
+      }
+      .recommend-body {
+        border-bottom: 1px solid #eee;
+        .recommend-item {
+          width:99%;
+          border-right: 1px solid #eee;
+          font-size: 12px;
+          text-align: center;
+        }
+      }
+    }
+  }
+</style>
+
