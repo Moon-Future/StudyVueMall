e655c1b2f1f737d8a73bb383f231e57386c08ea1
ChenLiang <236338364@qq.com>
2018-09-19 02:26:29 +0800

    mongoose

--- service/index.js
+++ service/index.js
@@ -1,5 +1,18 @@
 const Koa = require('koa')
 const app = new Koa()
+const mongoose = require('mongoose')
+const { connect, initSchemas } = require('./database/init.js')
+
+//立即执行函数
+;(async () =>{
+  await connect()
+  initSchemas()
+  const User = mongoose.model('User')
+  let oneUser = new User({userName:'ChenLiang-1', password:'8023'})
+  oneUser.save().then(() => {
+    console.log('插入成功')
+  })
+})()
 
 app.use(async (ctx) => {
   ctx.body = '<h1>hello Koa2</h1>'
